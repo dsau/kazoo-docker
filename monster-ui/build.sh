d0ff3b5cde81b91cf86128f0ff0f3adaf4f28b10
Damian Smyth <damian@dsau.co>
2016-08-26 21:02:14 +1000

    fix kamailio and monster-ui builds

--- monster-ui/build.sh
+++ monster-ui/build.sh
@@ -9,4 +9,7 @@ cp -a ~/.ssh etc/
 echo Using repository $REPO
 ../bin/get-commit $REPO > etc/commit
 echo Using repository $REPO commit: `cat etc/commit`
+if [ ! -e build/setup-private.sh ]; then
+	cp build/setup-private.sh.sample build/setup-private.sh
+fi
 docker build -t kazoo/monster-ui  --build-arg REPO=$REPO .
